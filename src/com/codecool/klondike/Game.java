a69d943192752b8afd0ba8b9318b9562f97d8bd7
akosVogel <vogelakos@gmail.com>
2018-06-27 11:12:47 +0200

    refill stock is done

--- src/com/codecool/klondike/Game.java
+++ src/com/codecool/klondike/Game.java
@@ -14,6 +14,7 @@ import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.Pane;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 
@@ -106,7 +107,14 @@ public class Game extends Pane {
     }
 
     public void refillStockFromDiscard() {
-        //TODO
+        if(stockPile.isEmpty()) {
+            for (Card card : discardPile.getCards()) {
+                stockPile.addCard(card);
+                card.flip();
+            }
+            stockPile.reversePile();
+            discardPile.clear();
+        }
         System.out.println("Stock refilled from discard pile.");
     }
 
